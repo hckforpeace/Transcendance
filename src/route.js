619736f0b59da43c94ef704f5cf2a8bd9b0551af
hckforpeace <pierbey@gmail.com>
2025-03-19 09:51:17 +0100

    work in progress on fastify server

---
+++ src/route.js
@@ -0,0 +1,18 @@
+// our-first-route.js
+
+/**
+ * Encapsulates the routes
+ * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
+ * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
+ */
+async function routes (fastify, options) {
+  fastify.get('/bite', async (request, reply) => {
+    return { hello: 'world' }
+  })
+}
+
+//ESM
+export default routes;
+
+// // CommonJs
+// module.exports = routes
