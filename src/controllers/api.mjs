7a9c40d83d4b5253fdc1a2605119efad0e82d4f9
Pablo Julian Jimenez Cano <pajimene@paul-f5Ar4s2.clusters.42paris.fr>
2025-04-02 13:31:41 +0200

    test

--- src/controllers/api.mjs
+++ src/controllers/api.mjs
@@ -3,7 +3,7 @@ const auth = async (req, reply) => {
     const user_data = { username: req.body.username, password: req.body.password };
     
     // Use the JWT functionality from the fastify instance
-    const token = await reply.jwtSign(user_data, { expiresIn: "17s" });
+    const token = await reply.jwtSign(user_data, { expiresIn: "1m" });
     
     return reply.status(200).send({ 
       message: 'Login successful', 
