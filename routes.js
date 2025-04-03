7dd27cfbd97cb3a4e4b11f5e13f7cb64d62c2668
Melanie Martin <melmarti@paul-f3Ar3s6.clusters.42paris.fr>
2025-04-03 16:42:41 +0200

    working on AI

--- routes.js
+++
@@ -1,8 +0,0 @@
-
-  async function routes(fastify, options) {
-    fastify.get('/data', async (request, reply) => {
-      const data = await getData();
-      const processed = await processData(data);
-      return processed;
-    });
-  }
