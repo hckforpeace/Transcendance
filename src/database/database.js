73d9320e55476f95bcc757247b031a65de92da55
name <tou@gmail.com>
2025-05-21 16:05:58 +0200

    last commit before merge

--- src/database/database.js
+++ src/database/database.js
@@ -24,8 +24,34 @@ export const initDB = async () => {
         matches_played INTEGER DEFAULT 0,
         player_id INTEGER NOT NULL,
         FOREIGN KEY (player_id) REFERENCES users(id) ON DELETE CASCADE
-)`);
+    )`);
 
+    await db.exec(`CREATE TABLE IF NOT EXISTS 2fa_code (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        code TEXT NOT NULL,
+        validity BOOLEAN NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        expired_at DATETIME,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    )`);
+
+    await db.exec(`CREATE TABLE IF NOT EXISTS tournament (
+        tournament_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        winner INTEGER, -- FK to users(id)
+        date DATETIME,
+        player1_id INTEGER,
+        player2_id INTEGER,
+        player1_alias TEXT,
+        player2_alias TEXT,
+        match1_id INTEGER,
+        match2_id INTEGER,
+        FOREIGN KEY (winner) REFERENCES users(id),
+        FOREIGN KEY (player1_id) REFERENCES users(id),
+        FOREIGN KEY (player2_id) REFERENCES users(id),
+        FOREIGN KEY (match1_id) REFERENCES matches(match_id),
+        FOREIGN KEY (match2_id) REFERENCES matches(match_id)
+    )`);
 
   } catch (error) {
     console.error('Error initializing the database:', error);
