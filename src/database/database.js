8b1015a3c05031f8499c9a6ebd03837e7b98ed70
hckforpeace <pierbey@gmail.com>
2025-06-13 15:43:12 +0200

    modified all foreign keys of users(id) to TEXT NOT NULL

--- src/database/database.js
+++ src/database/database.js
@@ -29,13 +29,13 @@ export const initDB = async () => {
         tournamentsWon INTEGER DEFAULT 0,
         tournamentsPlayed INTEGER DEFAULT 0,
         matchesPlayed INTEGER DEFAULT 0,
-        playerId INTEGER NOT NULL,
+        playerId TEXT NOT NULL,
         FOREIGN KEY (playerId) REFERENCES users(id) ON DELETE CASCADE
     )`);
 
     await db.exec(`CREATE TABLE IF NOT EXISTS "2fa_code" (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER NOT NULL,
+        user_id TEXT NOT NULL,
         code TEXT NOT NULL,
         validity BOOLEAN NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -47,8 +47,8 @@ export const initDB = async () => {
         tournament_id INTEGER PRIMARY KEY AUTOINCREMENT,
         winner INTEGER, -- FK to users(id)
         date DATETIME,
-        player1_id INTEGER,
-        player2_id INTEGER,
+        player1_id TEXT NOT NULL, 
+        player2_id TEXT NOT NULL,
         player1_alias TEXT,
         player2_alias TEXT,
         match1_id INTEGER,
@@ -64,8 +64,8 @@ export const initDB = async () => {
         match_id INTEGER PRIMARY KEY AUTOINCREMENT,
         player1_alias TEXT NOT NULL,
         player2_alias TEXT NOT NULL,
-        player1_id INTEGER NOT NULL,
-        player2_id INTEGER NOT NULL,
+        player1_id TEXT NOT NULL,
+        player2_id TEXT NOT NULL,
         player1_score INTEGER DEFAULT 0,
         player2_score INTEGER DEFAULT 0,
         date DATETIME DEFAULT CURRENT_TIMESTAMP,
