092cb6fbad7c9652245f4dbef42f1cf303581a4a
hckforpeace <pierbey@gmail.com>
2025-07-02 13:30:10 +0200

    Added env.sh

---
+++ srcs/secrets/env.sh
@@ -0,0 +1,15 @@
+
+export SMTP_USER="example@mail.com"
+export SMTP_PASS="SuperSecretPassword"
+export PROXY_SSL="on"
+export BACKEND="http://app:3000"
+export SSL_PORT="8845"
+export SERVER_NAME="localhost"
+export MODSEC_AUDIT_ENGINE="On"
+export MODSEC_AUDIT_LOG="/var/log/modsec_audit.log"
+export GF_SECURITY_ADMIN_PASSWORD="1234"
+export GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH="/etc/grafana/provisioning/dashboards/requests_count.json"
+export MAILER_USER="mppd.42.transcendence@gmail.com"
+export MAILER_PASS="txcj prjh exya uuop"
+
+
