debe9fb3d8df016b7e5796eef26a3ed401ddda8b
Jay Moker <dadamien78@gmail.com>
2025-07-02 10:58:19 +0200

    Added logout fix

--- srcs/requirements/app/src/typescript/logout.ts
+++ srcs/requirements/app/src/typescript/logout.ts
@@ -15,3 +15,9 @@ function logout() {
 		console.error("Logout error:", err);
 	});
 }
+
+window.addEventListener("beforeunload", function (e) {
+    // Do something
+    e.preventDefault(); // Prevents the default action
+    logout();
+});
